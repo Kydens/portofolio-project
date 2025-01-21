ba9938eaa7f1c2cccd9854eadc4352f74219d80a
Kydens <nathsyahed@gmail.com>
2025-01-21 18:25:07 +0700

    Change: z-index to 50

--- src/components/ComponentAlert.jsx
+++ src/components/ComponentAlert.jsx
@@ -3,7 +3,7 @@ import { IoAlertCircle } from 'react-icons/io5'
 
 const ComponentAlert = () => {
     return (
-        <div className="fixed bottom-0 right-0 m-2 w-fit h-fit z-40">
+        <div className="fixed bottom-0 right-0 m-2 w-fit h-fit z-50">
             <div className="alert w-60 md:w-80 h-fit py-3 px-6 rounded-xl bg-red-500">
                 <span className="text-white font-semibold flex gap-4 items-center text-sm">
                     <IoAlertCircle size={48} />
